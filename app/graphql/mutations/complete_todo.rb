72e261059369280fb482ddd53540e2f780947477
ezdar <ezdar2743@gmail.com>
2024-03-31 17:11:17 +0900

    fix: fix the error handling

--- app/graphql/mutations/complete_todo.rb
+++ app/graphql/mutations/complete_todo.rb
@@ -8,20 +8,14 @@ module Mutations
     def resolve(id:)
       todo = Todo.find_by(id: id)
       if todo.nil?
-        {
-          todo: nil,
-          errors: ["Todo not found."]
-        }
+        raise GraphQL::ExecutionError.new('Todo not found.', extensions: { code: 'TODO_NOT_FOUND' })
       elsif todo.update(completed: true)
         {
           todo: todo,
           errors: []
         }
       else
-        {
-          todo: nil,
-          errors: todo.errors.full_messages
-        }
+        raise GraphQL::ExecutionError.new('Unable to complete Todo.', extensions: { code: 'TODO_COMPLETE_FAILED', errors: todo.errors.full_messages })
       end
     end
   end
--- app/graphql/mutations/update_todo.rb
+++ app/graphql/mutations/update_todo.rb
@@ -1,23 +1,27 @@
 module Mutations
   class UpdateTodo < Mutations::BaseMutation
     argument :id, ID, required: true
-    argument :content, String, required: true
-    argument :completed, Boolean, required:true
+    argument :content, String, required: false
+    argument :completed, Boolean, required: false
 
     field :todo, Types::TodoType, null: false
     field :errors, [String], null: false
 
-    def resolve(id:, content:, completed:)
+    def resolve(id:, content: nil, completed: nil)
       todo = Todo.find_by(id: id)
 
       if todo.nil?
-        {todo: nil, errors: ["Todo not found."]}
-      elsif todo.update(content: content, completed: completed)
-        {todo: todo, errors: []}
-      else {
-        todo: nil,
-        errors: todo.errors.full_messages
-      }
+        raise GraphQL::ExecutionError.new('Todo not found.', extensions: { code: 'TODO_NOT_FOUND' })
+      end
+
+      update_params = {}
+      update_params[:content] = content unless content.nil?
+      update_params[:completed] = completed unless completed.nil?
+
+      if todo.update(update_params)
+        { todo: todo, errors: [] }
+      else
+        raise GraphQL::ExecutionError.new('Unable to update Todo.', extensions: { code: 'TODO_UPDATE_FAILED', errors: todo.errors.full_messages })
       end
     end
   end
